34139c241906fb05f78679baac851b9c80f66bef
Yallamaztar <budi.world@yahoo.com>
2026-02-24 00:34:03 +0200

    Better logger

--- internal/logger/log.go
+++ internal/logger/log.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -9,12 +10,28 @@ import (
 type Logger struct {
 	slug string
 	base *log.Logger
+	file *os.File
 }
 
-func New(slug string) *Logger {
+func New(slug, logPath string) *Logger {
+	writers := []io.Writer{os.Stdout}
+
+	var file *os.File
+	if logPath != "" {
+		var err error
+		file, err = os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err == nil {
+			writers = append(writers, file)
+		} else {
+			fmt.Printf("Failed to open log file %s: %v\n", logPath, err)
+		}
+	}
+
+	multi := io.MultiWriter(writers...)
 	return &Logger{
 		slug: slug,
-		base: log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile),
+		base: log.New(multi, "", log.LstdFlags|log.Lshortfile),
+		file: file,
 	}
 }
 
@@ -36,4 +53,12 @@ func (l *Logger) Fatal(v ...any) {
 
 func (l *Logger) Fatalf(format string, v ...any) {
 	l.base.Fatalf(l.prefix()+format, v...)
+}
+
+func (l *Logger) Warnf(format string, v ...any) {
+	l.base.Printf(l.prefix()+" [WARN] "+format+"\n", v...)
+}
+
+func (l *Logger) Warnln(v ...any) {
+	l.base.Println(append([]any{l.prefix(), "[WARN]"}, v...)...)
 }
